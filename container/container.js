f4bacf5f3c982755261efff358bb3115d8f00e10
lovelock <frostwong@gmail.com>
2015-08-09 10:55:28 -0400

    cross domain between parent and child

--- container/container.js
+++ container/container.js
@@ -1,12 +1,20 @@
 window.onload = function () {
+    var btn = document.getElementById("send");
     function receiveMessage(e) {
         console.log(e.origin);
-        if (e.origin !== "http://iframe1.unixera.com") {
+        if (e.origin !== "http://iframe2.new-unixera.com") {
             return;
         }
 
         document.getElementById("message").innerHTML = e.data;
     }
+
+    function sendMessage() {
+        var message_to_send = document.getElementById("message_to_send").value;
+        document.getElementById("receiver").contentWindow.postMessage(message_to_send, "http://iframe2.new-unixera.com");
+    }
+
+    btn.addEventListener("click", sendMessage);
 
     addEventListener("message", receiveMessage, false);
 }
--- iframe2/iframe2.js
+++ iframe2/iframe2.js
@@ -1,5 +1,6 @@
 window.onload = function () {
 	var msg = document.getElementById('message');
+    var btn = document.getElementById("send");
 
 	function receiveMessage(e) {
         console.log(e.data);
@@ -10,5 +11,11 @@ window.onload = function () {
 		msg.innerHTML = e.data;
 	}
 
+    function sendMessage() {
+        var message_to_send = document.getElementById("message_to_send").value;
+        window.parent.postMessage(message_to_send, "http://container.unixera.com");
+    }
+
+    btn.addEventListener('click', sendMessage, false);
     addEventListener('message', receiveMessage, false);
 }
