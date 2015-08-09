dfb0a76aa34c12765b7d92976185f17576ec9a8a
root <root@debian.unixera.com>
2015-08-09 13:56:03 -0400

    Nothing to say

--- iframe1/iframe1.js
+++ iframe1/iframe1.js
@@ -1,18 +1,3 @@
-window.onload = function () {
-	var msg = document.getElementById('message');
-
-	function receiveMessage(e) {
-        console.log(e.data);
-		if (e.origin !== 'http://container.unixera.com') {
-			return;
-		}
-
-		msg.innerHTML = e.data;
-	}
-
-    function globalFunction() {
-        alert("I received message to iframe2");
-    }
-
-    addEventListener('message', receiveMessage, false);
+function say() {
+    alert("I am alerting");
 }
--- iframe2/iframe2.js
+++ iframe2/iframe2.js
@@ -1,5 +1,3 @@
 window.onload = function () {
-    location.href="http://iframe1.unixera.com/iframe1.html";
-    console.log("redirect");
-    globalFunction();
+    document.getElementById("grand-child").say();
 }
